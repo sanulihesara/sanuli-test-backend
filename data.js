11daa72ef8e783795ee3ab75496df4a3a77b29e7
Sanuli <sanulicooray84@gmail.com>
2025-01-27 21:24:24 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,23 @@
+const aboutMeData = {
+    name: "Sanuli",
+    age: "14",
+    location: "Wijerama",
+    school: "Musaeus College Colombo 07",
+    hobbies: [
+      "Web Development",
+      "Photography",
+      "Playing Guitar and Piano",
+      "Traveling",
+      "Reading Sherlock Holmes"
+    ],
+    description: `
+      Hello! My name is Sanuli. I am 14 years old and passionate about web 
+      development and technology. I live in Wijerama. I love animals and I 
+      have a dog and a cat. I go to Musaeus College Colombo 07 and I also 
+      play badminton. I have multiple hobbies that I really love and enjoy by 
+      doing them...
+    `,
+  };
+  
+  module.exports = aboutMeData;
+  
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const aboutMeData = require("./data");
+
+const app = express();
+const PORT = 5002;
+
+app.use(cors());
+
+app.get("/about", (req, res) => {
+  res.json(aboutMeData);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
